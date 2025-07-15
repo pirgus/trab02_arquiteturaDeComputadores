c73df4f410fd649f55a9045e36aaa9c119b71bef
pirgus <jaquecavaller@gmail.com>
2025-07-15 08:15:01 -0300

    script para rodar os códigos de friendly numbers

---
+++ friendly/run_friendly_codes.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Lista de entradas para os programas friendly_*
+inputs=("250000" "500000" "750000" "1000000")
+
+# Executa friendly_seq.x e friendly_par.x com entradas
+for exe in "friendly_seq" "friendly_par"; do
+    echo "Executando $exe com diferentes entradas..."
+    for value in "${inputs[@]}"; do
+        echo "Entrada: 1 $value"
+        for i in {1..10}; do
+            echo "Execução $i:"
+            echo -e "1 $value\n0 0" | ./$exe
+        done
+        echo ""
+    done
+    echo ""
+done
+
